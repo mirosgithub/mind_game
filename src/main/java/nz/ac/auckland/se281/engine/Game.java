4eaa541e04462d9865a306fdf0a5325f270d4021
Suah Kim <skim460@aucklanduni.ac.nz>
2025-05-12 17:20:34 +1200

    feat: read and validate colour input

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,12 +2,16 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.cli.Utils;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
 
   private int numRounds;
   private int currentRound;
+  private Colour playerColour = null;
+  private Colour playerGuess = null;
 
   public Game() {}
 
@@ -20,14 +24,37 @@ public class Game {
 
   public void play() {
     currentRound++;
-    
+
     // display round
     MessageCli.START_ROUND.printMessage(currentRound, numRounds);
 
-    // prompt for colors
+    // prompt for colours
     MessageCli.ASK_HUMAN_INPUT.printMessage();
-    
+
+    // get colour input
+    getColourInput();
+
+    // validate input
+    while (playerColour == null || playerGuess == null) {
+      // print error message
+      MessageCli.INVALID_HUMAN_INPUT.printMessage();
+
+      // get colour input again
+      getColourInput();
+    }
   }
 
   public void showStats() {}
+
+  private void getColourInput() {
+    // read input
+    String input = Utils.scanner.nextLine();
+    String[] inputs = input.split(" ");
+
+    // convert to Colour enum
+    if (inputs.length == 2) {
+      playerColour = Colour.fromInput(inputs[0]);
+      playerGuess = Colour.fromInput(inputs[1]);
+    }
+  }
 }
